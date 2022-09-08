72c47b74830c93ad7cdfb83df59139406fbb610c
Saidabdulla <abdullamasharipov11@gmail.com>
2022-09-08 21:35:58 +0500

    Basic settings

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello React app</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
