8a5a52289994347780c5c946329b30e1b53c33be
Saidabdulla <abdullamasharipov11@gmail.com>
2022-09-10 00:28:57 +0500

    Navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import { Query } from "@apollo/client/react/components";
-import { GET_CATEGORIES } from "../../graphql/queries";
+import { GET_CATEGORIES, GET_CURRENCIES } from "../../graphql/queries";
 
 import styles from "./navbar.module.css";
 import { ReactComponent as IconLogo } from "../../assets/img/VSF.svg";
+import { ReactComponent as IconCurrency } from "../../assets/img/dollar_and_carret.svg";
+import { ReactComponent as IconCart } from "../../assets/img/empty_cart.svg";
 
 class Navbar extends Component {
   render() {
@@ -19,7 +21,7 @@ class Navbar extends Component {
 
                 const { categories } = data;
                 return categories.map((cat) => (
-                  <NavLink className={styles.link} to={cat.name}>
+                  <NavLink key={cat.name} className={styles.link} to={cat.name}>
                     {cat.name}
                   </NavLink>
                 ));
@@ -31,10 +33,36 @@ class Navbar extends Component {
             <IconLogo />
           </NavLink>
 
-          <ul className={styles.right}>
-            <li>$</li>
-            <li>savat</li>
-          </ul>
+          <div className={styles.right}>
+            <button type="button" className={styles["currency-btn"]}>
+              <IconCurrency />
+            </button>
+            <button type="button" className={styles["cart-btn"]}>
+              <IconCart />
+            </button>
+
+            <ul className={styles["currency-modal"]}>
+              <Query query={GET_CURRENCIES}>
+                {({ loading, error, data }) => {
+                  if (error) return error;
+                  if (loading || !data) return "loading...";
+
+                  const { currencies } = data;
+                  return currencies.map((cy) => (
+                    <li
+                      className={cy.symbol === "$" ? "currency-active" : ""}
+                      key={cy.symbol}
+                      onClick={() => console.log(cy.symbol)}
+                    >
+                      {cy.symbol} {cy.label}
+                    </li>
+                  ));
+                }}
+              </Query>
+            </ul>
+
+            {/* <div className={styles["currency-modal"]}></div> */}
+          </div>
         </div>
 
         <Outlet />
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,9 +1,18 @@
 import { gql } from "@apollo/client";
 
 export const GET_CATEGORIES = gql`
-  query GetLocations {
+  query GetCategories {
     categories {
       name
+    }
+  }
+`;
+
+export const GET_CURRENCIES = gql`
+  query GetCurrencies {
+    currencies {
+      label
+      symbol
     }
   }
 `;
